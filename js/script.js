cba99c13c649802aecdc5e4e63fade93e5fc6713
Glitch (hello-webpage) <none>
2021-02-08 18:23:32 +0100

    Rajout Mentions Légales + Responsive + Menu

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function load() {
+  var burger = document.querySelector("nav div i");
+  var ul = document.querySelector("nav ul");
+  burger.addEventListener('click', function menu() {
+    ul.class
+    if (ul.classList.contains('mobile-nav')){
+      ul.classList.remove('mobile-nav');
+      burger.classList.replace("fa-times", "fa-bars");
+    }
+    else {
+      ul.classList.add('mobile-nav');
+      burger.classList.replace("fa-bars", "fa-times");
+    }
+  })
+
+});
